76b6c9ad086c825fc57125ff26373593f9b06266
Connorb731 <connorbriggs@pluginpromotions.com>
2025-05-25 18:02:56 -0400

    Fix: force Tailwind CSS to bundle by injecting dummy styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    {/* Dummy Tailwind styles to ensure CSS is bundled during Vite build */}
+    <div className="hidden text-blue-500 bg-red-500 p-4 m-2"></div>
+  </>
+);
+
