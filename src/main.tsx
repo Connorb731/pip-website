82b726a6ccd888a9e60d7c3ed2bb88020158989e
Connorb731 <connorbriggs@pluginpromotions.com>
2025-05-26 16:02:18 -0400

    Fix wrap App and dummy div in root element to avoid blank screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <div>
     <App />
     {/* Dummy Tailwind styles to ensure CSS is bundled during Vite build */}
     <div className="hidden text-blue-500 bg-red-500 p-4 m-2"></div>
-  </>
+  </div>
 );
-
