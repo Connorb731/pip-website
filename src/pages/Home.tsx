b0c9fbb1cdada12f5931ac8a1d80ab8cda308fa0
Connorb731 <connorbriggs@pluginpromotions.com>
2025-05-26 16:13:26 -0400

    Test: render HeroSection only to debug component output

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,10 @@
-import { useEffect } from 'react';
 import HeroSection from '@/components/HeroSection';
-import ProductShowcase from '@/components/ProductShowcase';
-import HowItWorks from '@/components/HowItWorks';
-import Benefits from '@/components/Benefits';
-import CustomerCarousel from '@/components/CustomerCarousel';
-import TestimonialsSection from '@/components/TestimonialsSection';
-import ContactSection from '@/components/ContactSection';
-import TeamSection from '@/components/TeamSection';
 
-const Home = () => {
-  useEffect(() => {
-    // Set page title
-    document.title = 'Plug-In Promotions | Free Charging Stations for Bars & Restaurants';
-  }, []);
-
+export default function Home() {
   return (
-    <>
+    <div className="text-white text-3xl p-6 bg-blue-700">
       <HeroSection />
-      <CustomerCarousel />
-      <ProductShowcase />
-      <HowItWorks />
-      <Benefits />
-      <TestimonialsSection />
-      <ContactSection />
-      <TeamSection />
-    </>
+    </div>
   );
-};
+}
 
-export default Home;
